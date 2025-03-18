2615df0d5bf14305246d117e2cd58ab05d066cf9
taokepppooo <1075626521@qq.com>
2025-03-18 11:52:18 +0800

    feat: 添加静态链表

--- StaticLinkList.c
+++ StaticLinkList.c
@@ -27,6 +27,24 @@ int Malloc_SLL(StaticLinkList space)
   return i;
 }
 
+void Free_SLL(StaticLinkList space, int k)
+{
+  space[k].cur = space[0].cur;
+  space[0].cur = k;
+}
+
+int listLength(StaticLinkList L)
+{
+  int j = 0;
+  int i = L[MAXSIZE - 1].cur;
+  while(i)
+  {
+    j++;
+    i = L[i].cur;
+  }
+  return j;
+}
+
 void listInsert(StaticLinkList L, int i, int e)
 {
   int j, k, l;
@@ -40,6 +58,13 @@ void listInsert(StaticLinkList L, int i, int e)
   j = Malloc_SLL(L);
   if(j)
   {
+    L[j].data = e;
+    for(l = 1; l <= i - 1; l++)
+    {
+      k = L[k].cur;
+    }
+    L[j].cur = L[k].cur;
+    L[k].cur = j;
   }
 }
 
@@ -53,15 +78,44 @@ void listDelete(StaticLinkList L, int i)
   }
 
   k = MAXSIZE - 1;
+
+  for(j = 1; j <= i - 1; j++)
+  {
+    k = L[k].cur;
+  }
+
+  j = L[k].cur;
+  L[k].cur = L[j].cur;
+  Free_SLL(L, j);
 }
 
-void Free_SLL(StaticLinkList space, int k)
+void printfList(StaticLinkList L)
 {
-  space[k].cur = space[0].cur;
-  space[0].cur = k;
+  int i = L[MAXSIZE - 1].cur;
+  while(i)
+  {
+    printf("%d ", L[i].data);
+    i = L[i].cur;
+    printf("cur: %d\n", L[i].cur);
+  }
+  printf("\n");
 }
 
 int main(int argc, char *argv[])
 {
+  StaticLinkList L;
+  initSpace(L);
+  printf("初始化完成\n");
+  printfList(L);
+  listInsert(L, 1, 1);
+  listInsert(L, 1, 2);
+  listInsert(L, 1, 3);
+  listInsert(L, 1, 4);
+  listInsert(L, 1, 5);
+  printf("插入完成\n");
+  printfList(L);
+  listDelete(L, 3);
+  printf("删除完成\n");
+  printfList(L);
   return 0;
 }
